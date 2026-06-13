89afacd0407924e34e64bcab7993c5566858676f
solvarhuynh <solvarhuynh@gmail.com>
2026-06-13 16:22:28 +0700

    delect carpla data

--- test.R
+++
@@ -1,7 +0,0 @@
-con <- DBI::dbConnect(RSQLite::SQLite(),dbname = "./web_scraping/data/master_data.db")
-DBI::dbListTables(con)
-DBI::dbListFields(con, "car_listings")
-query <- "SELECT `price`, `source`, `url` FROM car_listings WHERE `price` > 10000000000"
-test <- DBI::dbGetQuery(con,query)
-print(test)
-# DBI::dbDisconnect(con
